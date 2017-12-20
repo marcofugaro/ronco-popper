da0f343bdb619e9b00af521aab78ad563a50d97a
Marco Fugaro <marco.fugaro@gmail.com>
2017-12-20 18:55:33 +0100

    🌌  Fix spacings

--- app/js/main.js
+++ app/js/main.js
@@ -4,27 +4,27 @@ import mouth from './mouth'
 
 // show the hint after a few second
 setTimeout(() => {
-    if(document.querySelector('.hint'))
-        document.querySelector('.hint').classList.add('visible')
+  if (document.querySelector('.hint'))
+    document.querySelector('.hint').classList.add('visible')
 }, 3000)
 
 // play the sound of the ronco button
 const roncoSound = document.getElementById('ronco-sound')
-document.querySelector('.ronco-button').addEventListener('click', function() {
-    roncoSound.currentTime = 0
-    roncoSound.play()
+document.querySelector('.ronco-button').addEventListener('click', () => {
+  roncoSound.currentTime = 0
+  roncoSound.play()
 })
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js', { scope: './' })
-        .then(function(reg) {
-            console.log('Service Worker registration successful: ', reg)
-        })
-        .catch(function(err) {
-            console.error('Service Worker registration failed: ', err)
-        })
-
-    navigator.serviceWorker.ready.then(function(reg) {
-       console.log('Service Worker ready: ', reg)
+  navigator.serviceWorker.register('/sw.js', { scope: './' })
+    .then((reg) => {
+      console.log('Service Worker registration successful: ', reg)
     })
+    .catch((err) => {
+      console.error('Service Worker registration failed: ', err)
+    })
+
+  navigator.serviceWorker.ready.then((reg) => {
+    console.log('Service Worker ready: ', reg)
+  })
 }
--- app/js/mouth.js
+++ app/js/mouth.js
@@ -3,70 +3,70 @@ import './vendor/MorphSVGPlugin.min'
 import hand from './hand'
 
 class Mouth {
-    el = document.querySelectorAll('.mouth-up, .mouth-down')
+  el = document.querySelectorAll('.mouth-up, .mouth-down')
 
-    tl = null
-    isSucking = false
-    mouthX = 0
-    lipProgress = 0
+  tl = null
+  isSucking = false
+  mouthX = 0
+  lipProgress = 0
 
-    gaggingSound = document.getElementById('gagging-sound')
-    popSound = document.getElementById('pop-sound')
+  gaggingSound = document.getElementById('gagging-sound')
+  popSound = document.getElementById('pop-sound')
 
-    constructor() {
-        const duration = 0.4
-        const ease = Power3.easeOut
+  constructor() {
+    const duration = 0.4
+    const ease = Power3.easeOut
 
-        this.tl = new TimelineMax({ paused: true })
-            .add('open')
-            .to('.mouth-open .lip-black-lower', duration, { morphSVG: '.mouth-gagging .lip-black-lower', ease: ease }, 'open')
-            .to('.mouth-open .lip-black-upper', duration, { morphSVG: '.mouth-gagging .lip-black-upper', ease: ease }, 'open')
-            .to('.mouth-open .lip-red-lower', duration, { morphSVG: '.mouth-gagging .lip-red-lower', ease: ease }, 'open')
-            .to('.mouth-open .lip-red-upper', duration, { morphSVG: '.mouth-gagging .lip-red-upper', ease: ease }, 'open')
-            .to('.mouth-open .outline', duration, { morphSVG: '.mouth-gagging .outline', ease: ease }, 'open')
-            .to('.mouth-open .teeth', duration, { morphSVG: '.mouth-gagging .teeth', ease: ease }, 'open')
-            .add('gagging')
-            .to('.mouth-open .lip-black-lower', duration, { morphSVG: '.mouth-sucking .lip-black-lower' }, 'gagging')
-            .to('.mouth-open .lip-black-upper', duration, { morphSVG: '.mouth-sucking .lip-black-upper' }, 'gagging')
-            .to('.mouth-open .lip-red-lower', duration, { morphSVG: '.mouth-sucking .lip-red-lower' }, 'gagging')
-            .to('.mouth-open .lip-red-upper', duration, { morphSVG: '.mouth-sucking .lip-red-upper' }, 'gagging')
-            .to('.mouth-open .teeth', duration, { morphSVG: '.mouth-sucking .teeth' }, 'gagging')
-            .add('sucking')
-    }
+    this.tl = new TimelineMax({ paused: true })
+      .add('open')
+      .to('.mouth-open .lip-black-lower', duration, { morphSVG: '.mouth-gagging .lip-black-lower', ease: ease }, 'open')
+      .to('.mouth-open .lip-black-upper', duration, { morphSVG: '.mouth-gagging .lip-black-upper', ease: ease }, 'open')
+      .to('.mouth-open .lip-red-lower', duration, { morphSVG: '.mouth-gagging .lip-red-lower', ease: ease }, 'open')
+      .to('.mouth-open .lip-red-upper', duration, { morphSVG: '.mouth-gagging .lip-red-upper', ease: ease }, 'open')
+      .to('.mouth-open .outline', duration, { morphSVG: '.mouth-gagging .outline', ease: ease }, 'open')
+      .to('.mouth-open .teeth', duration, { morphSVG: '.mouth-gagging .teeth', ease: ease }, 'open')
+      .add('gagging')
+      .to('.mouth-open .lip-black-lower', duration, { morphSVG: '.mouth-sucking .lip-black-lower' }, 'gagging')
+      .to('.mouth-open .lip-black-upper', duration, { morphSVG: '.mouth-sucking .lip-black-upper' }, 'gagging')
+      .to('.mouth-open .lip-red-lower', duration, { morphSVG: '.mouth-sucking .lip-red-lower' }, 'gagging')
+      .to('.mouth-open .lip-red-upper', duration, { morphSVG: '.mouth-sucking .lip-red-upper' }, 'gagging')
+      .to('.mouth-open .teeth', duration, { morphSVG: '.mouth-sucking .teeth' }, 'gagging')
+      .add('sucking')
+  }
 
-    startSucking() {
-        this.isSucking = true
+  startSucking() {
+    this.isSucking = true
 
-        this.tl.timeScale(1).tweenTo('gagging')
-    }
+    this.tl.timeScale(1).tweenTo('gagging')
+  }
 
-    stopSucking() {
-        this.isSucking = false
+  stopSucking() {
+    this.isSucking = false
 
-        this.popSound.currentTime = 0
-        this.popSound.play()
+    this.popSound.currentTime = 0
+    this.popSound.play()
 
-        this.tl.timeScale(2).tweenTo('open')
+    this.tl.timeScale(2).tweenTo('open')
 
-        clearInterval(hand.popCheckInterval)
-        hand.popCheckInterval = null
-        hand.oldKeyframeX = hand.keyframeX = 0
+    clearInterval(hand.popCheckInterval)
+    hand.popCheckInterval = null
+    hand.oldKeyframeX = hand.keyframeX = 0
 
-        hand.isLerping = true
-        setTimeout(() => { hand.isLerping = false }, 500)
+    hand.isLerping = true
+    setTimeout(() => { hand.isLerping = false }, 500)
 
-        TweenMax.to(this.el, hand.easeStrength, { x: 0 })
-    }
+    TweenMax.to(this.el, hand.easeStrength, { x: 0 })
+  }
 
-    followFinger() {
-        // let's move th mouth
-        this.mouthX = - Math.pow(hand.triggerX - hand.totalDistanceX, 1/2.5) * 1.2
-        TweenMax.to(this.el, hand.easeStrength, { x: this.mouthX })
+  followFinger() {
+    // let's move th mouth
+    this.mouthX = - Math.pow(hand.triggerX - hand.totalDistanceX, 1/2.5) * 1.2
+    TweenMax.to(this.el, hand.easeStrength, { x: this.mouthX })
 
-        // let's close the mouth more
-        this.lipProgress = Math.pow(hand.triggerX - hand.totalDistanceX, 1/2.5) * 2
-        this.tl.progress(0.5 + this.lipProgress / 100)
-    }
+    // let's close the mouth more
+    this.lipProgress = Math.pow(hand.triggerX - hand.totalDistanceX, 1/2.5) * 2
+    this.tl.progress(0.5 + this.lipProgress / 100)
+  }
 }
 
 export default new Mouth()
